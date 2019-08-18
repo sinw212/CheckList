249ccd8d3c2dc29e510cbac2353d5dae6fadd16e
홍승아 <rrlooa1090@naver.com>
2019-08-19 02:35:03 +0900

    daily

--- CLFP4/app/src/main/java/com/example/clfp4/AlarmReceiver.java
+++ CLFP4/app/src/main/java/com/example/clfp4/AlarmReceiver.java
@@ -1,4 +1,12 @@
 package com.example.clfp4;
 
-public class AlarmReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    public void onReceive(Context context, Intent intent){
+
+    }
 }
